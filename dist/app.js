2f5fff4910ab39c74d641f4dd18844d652999465
joao felipe <jfelipepassos@gmail.com>
2023-06-04 03:18:48 -0300

    adicionar tipagem nas funções de requisição e resposta

--- dist/app.js
+++ dist/app.js
@@ -5,9 +5,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
 const port = 3000;
 app.get('/', (req, res) => {
     res.send('API de retorno');
+});
+app.post('/', (req, res) => {
+    console.log(req.body.cliente);
+    res.status(200).json({ message: 'Requisição POST recebida' });
 });
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
 import express from 'express';
+import {Request, Response} from 'express';
 
 const app = express();
+app.use(express.json());
 const port: number = 3000;
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('API de retorno');
+});
+
+app.post('/', (req: Request, res: Response) => {
+    console.log(req.body.cliente);
+    res.status(200).json({ message: 'Requisição POST recebida' });
 });
 
 app.listen(port, () => {
